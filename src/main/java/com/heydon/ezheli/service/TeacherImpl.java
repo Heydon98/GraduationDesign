d8f464d70e55eb6f7b8a6389d4837230d55cd371
Heydon98 <heydon_mail@163.com>
2020-05-11 09:05:47 +0800

    完成教师端登陆

--- src/main/java/com/heydon/ezheli/service/TeacherImpl.java
+++ src/main/java/com/heydon/ezheli/service/TeacherImpl.java
@@ -1,11 +1,18 @@
 package com.heydon.ezheli.service;
 
 import com.heydon.ezheli.dao.TeacherDao;
+import com.heydon.ezheli.util.JwtUtil;
 import com.heydon.ezheli.util.ResultUtil;
+import com.heydon.ezheli.util.RetCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.Map;
 
+@Transactional
+@Service
 public class TeacherImpl implements TeacherService{
 
     @Autowired
@@ -17,8 +24,18 @@ public class TeacherImpl implements TeacherService{
         String password = map.get("password");
         Map result = teacherDao.findTeacByTeacIdAndPassword(username, password);
         if (result != null) {
-            
+            int collegeId = (int) result.get("college_id");
+            long teacId = (long) result.get("teac_id");
+            String realName = (String) result.get("name");
+            String token = JwtUtil.createToken(username, collegeId);
+            Map<String, Object> data = new HashMap<>();
+            data.put("username", teacId);
+            data.put("collegeId", collegeId);
+            data.put("realName", realName);
+            data.put("token", token);
+            return new ResultUtil(RetCode.SUCCESS.getCode(), "success", data);
+        } else {
+            return new ResultUtil(RetCode.FAIL.getCode(), "用户名或密码错误");
         }
-        return null;
     }
 }
